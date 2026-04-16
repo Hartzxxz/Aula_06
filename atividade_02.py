44917e619a1a96454841d7316635a5decaa8ac5e
Hartzxxz <cansado2339@gmail.com>
2026-04-16 18:32:18 -0300

    aula_0600

--- atividade_02.py
+++ atividade_02.py
@@ -5,16 +5,14 @@ while n == 'S':
     venda = float(input('Digite o valor das vendas: '))
 
     soma += venda
-
     n  = input('Quer continuar? [S/N]').upper().strip()
 
-    if soma >= 1000:
+if soma >= 1000:
         desconto = soma * 0.10
-        
         total = soma - desconto
 
         print(f'O valor total com desconto foi de: {soma:.2f}')
         print(f'O desconto foi de: {desconto:.2f}')
-    else:
+else:
         print(f'O valor das vendas foi de R$: {soma:.2f}')
         print(f'nao teve desconto')
